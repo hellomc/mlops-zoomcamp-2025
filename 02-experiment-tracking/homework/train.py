74fcff18c320ac18a9e841b18e7fdca54e63de3f
hellomc <mc.adea@gmail.com>
2025-05-22 16:23:50 -0400

    Updated python file with mlflow commands

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import root_mean_squared_error
@@ -17,6 +18,7 @@ def load_pickle(filename: str):
     default="./output",
     help="Location where the processed NYC taxi trip data was saved"
 )
+
 def run_train(data_path: str):
 
     X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
@@ -28,6 +30,23 @@ def run_train(data_path: str):
 
     rmse = root_mean_squared_error(y_val, y_pred)
 
+    mlflow.log_metric('rmse', rmse)
+    # Save the model
+    mlflow.sklearn.log_model(rf, artifact_path='models_mlflow')
+
 
 if __name__ == '__main__':
-    run_train()
+    # Set the tracking URI to remote server PUBLIC IP
+    mlflow.set_tracking_uri('http://ec2-18-116-14-12.us-east-2.compute.amazonaws.com:5000')
+
+    mlflow.set_experiment('nyc-taxi-experiment-2023')
+
+    with mlflow.start_run():
+        mlflow.set_tag('model', 'RandomForestRegressor')
+        mlflow.log_param('max_depth', 10)
+        mlflow.log_param('random_state', 0)
+        
+        run_train()
+
+        
+            
